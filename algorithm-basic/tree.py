de83faf9840b2a81447201e7b8c44e46761b0041
Eshin Kunishima <ek@esh.ink>
2017-04-19 00:37:20 +0900

    [Algorithm Basic] add basic class for tree

---
+++ algorithm-basic/tree.py
@@ -0,0 +1,5 @@
+class Node(object):
+    def __init__(self, value = None, leftChild = None, rightChild = None):
+        self.value = value
+        self.leftChild = leftChild
+        self.rightChild = rightChild
