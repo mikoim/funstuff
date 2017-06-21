efaeb6e4e6835c541d24acb2bde90be4e4f5b833
Eshin Kunishima <ek@esh.ink>
2017-06-21 23:14:50 +0900

    [mercari Summer Internship 2017 Qualification] add Item model

--- db/models.py
+++ db/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Item(models.Model):
+    name = models.TextField()
+    title = models.TextField()
+    description = models.TextField()
+    price = models.IntegerField()
+    pv = models.IntegerField()
+    status = models.BooleanField()
+
+    def __str__(self) -> str:
+        return f'{self.name}'
