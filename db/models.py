ff654918de2532e569caa76f9820ec75b126af4d
Eshin Kunishima <ek@esh.ink>
2017-06-22 23:14:34 +0900

    [mercari Summer Internship 2017 Qualification] allow blank in some fields

--- db/models.py
+++ db/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Item(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
-    name = models.TextField()
-    title = models.TextField()
-    description = models.TextField()
+    name = models.TextField(blank=True)
+    title = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     price = models.IntegerField()
-    pv = models.IntegerField(default=0)
+    pv = models.IntegerField()
     status = models.BooleanField()
 
     def __str__(self) -> str:
