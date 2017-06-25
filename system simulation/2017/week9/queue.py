65302d85106b3b2cb40ff2b250b6ae04b0620bcd
Eshin Kunishima <ek@esh.ink>
2017-06-26 01:49:04 +0900

    [System Simulation] add static arrival simulation

--- system simulation/2017/week9/queue.py
+++ system simulation/2017/week9/queue.py
@@ -1,17 +1,15 @@
+import itertools
+import json
 import random
 import statistics
-import itertools
 
 
-def sim(customer_per_slice=6, time_slice=600, duration=17 * 60, max_seat=9):
-    # variables
-    time_line = [0] * 3600
-    queue = []
-    seat = []
-    leaver = 0
-    result = []
+def arrival_static(time_line: list, time_slice: int, customer_per_slice: int):
+    for i in range(time_slice, len(time_line), time_slice):
+        time_line[i] += customer_per_slice
 
-    # step 1
+
+def arrival_random(time_line: list, time_slice: int, customer_per_slice: int):
     full_fill_count = len(time_line) // time_slice
     for n in range(full_fill_count):
         for _ in range(customer_per_slice):
@@ -28,6 +26,18 @@ def sim(customer_per_slice=6, time_slice=600, duration=17 * 60, max_seat=9):
             r = random.randint(a, b)
             if r <= len(time_line) - 1:
                 time_line[r] += 1
+
+
+def sim(customer_per_slice=6, time_slice=600, duration=17 * 60, max_seat=9, arrival_function=arrival_random):
+    # variables
+    time_line = [0] * 3600
+    queue = []
+    seat = []
+    leaver = 0
+    result = []
+
+    # step 1
+    arrival_function(time_line, time_slice, customer_per_slice)
 
     # step 2
     t = 0
@@ -66,17 +76,51 @@ def sim(customer_per_slice=6, time_slice=600, duration=17 * 60, max_seat=9):
 
 
 if __name__ == '__main__':
-    for n in range(2, 11, 1):
-        l = [sim(n) for _ in range(10000)]
+    # # Problem 1
+    # for n in range(2, 11, 1):
+    #     l = [sim(n) for _ in range(10000)]
+    #
+    #     max_queue_len = max(map(max, l))
+    #     min_queue_len = min(map(min, l))
+    #     mean_queue_len = statistics.mean(map(statistics.mean, l))
+    #
+    #     max_closed_time = max(map(len, l))
+    #     min_closed_time = min(map(len, l))
+    #     mean_closed_time = statistics.mean(map(len, l))
+    #
+    #     mean_queue_list = list(map(statistics.mean, itertools.zip_longest(*l, fillvalue=0)))
+    #
+    #     print(n, max_queue_len, mean_queue_len, min_queue_len, max_closed_time, mean_closed_time, min_closed_time, mean_queue_list)
 
-        max_queue_len = max(map(max, l))
-        min_queue_len = min(map(min, l))
-        mean_queue_len = statistics.mean(map(statistics.mean, l))
+    # Problem 2
+    # for consumer, duration, seat in itertools.product(range(8, 11, 2), range(2 * 60, 21 * 60, 120), range(2, 11, 2)):
+    #     l = [sim(consumer, duration=duration, max_seat=seat) for _ in range(1000)]
+    #
+    #     max_queue_len = max(map(max, l))
+    #     min_queue_len = min(map(min, l))
+    #     mean_queue_len = statistics.mean(map(statistics.mean, l))
+    #
+    #     max_closed_time = max(map(len, l))
+    #     min_closed_time = min(map(len, l))
+    #     mean_closed_time = statistics.mean(map(len, l))
+    #
+    #     print(consumer, duration, seat, max_queue_len, mean_queue_len, min_queue_len, max_closed_time, mean_closed_time, min_closed_time)
 
-        max_closed_time = max(map(len, l))
-        min_closed_time = min(map(len, l))
-        mean_closed_time = statistics.mean(map(len, l))
-
-        mean_queue_list = list(map(statistics.mean, itertools.zip_longest(*l, fillvalue=0)))
-
-        print(n, max_queue_len, mean_queue_len, min_queue_len, max_closed_time, mean_closed_time, min_closed_time, mean_queue_list)
+    # Problem 1 static
+    # for n in range(2, 11, 1):
+    #     l = [sim(1, time_slice=600 // n, arrival_function=arrival_static) for _ in range(1)]
+    #
+    #     max_queue_len = max(map(max, l))
+    #     min_queue_len = min(map(min, l))
+    #     mean_queue_len = statistics.mean(map(statistics.mean, l))
+    #
+    #     max_closed_time = max(map(len, l))
+    #     min_closed_time = min(map(len, l))
+    #     mean_closed_time = statistics.mean(map(len, l))
+    #
+    #     mean_queue_list = list(map(statistics.mean, itertools.zip_longest(*l, fillvalue=0)))
+    #
+    #     print(n, max_queue_len, mean_queue_len, min_queue_len, max_closed_time, mean_closed_time, min_closed_time)
+    #
+    #     with open('static/{:d}.json'.format(n), mode='w') as fp:
+    #         json.dump(mean_queue_list, fp)
